416bc3dd306074d480ac12474bce38702cb96750
saurabh singh <ss1380820@gmail.com>
2026-01-29 21:46:41 +0530

    mock interview 11

---
+++ index10.js
@@ -0,0 +1,139 @@
+// a= ["2","4","8","10"]
+// function missing(a){
+//     return a.reverse("")
+
+// }
+
+// console.log(missing(a))
+
+
+// function debounce(fn,delay){
+
+//     let timer;
+//     return function(){
+
+//         timer=setTimeout(()=>{
+//             clearTimeout(timer)
+//             fn()
+
+//         },delay)
+
+//     }
+
+// }
+// const ans=debounce(function(){
+//     console.log("hello")
+// },700)
+// ans()
+
+
+
+
+
+// function throttle(fn,delay){
+
+//     let lastTime=0;
+
+//     return function(){
+//         const now=Date.now()
+        
+//         if(now-lastTime>=delay){
+//             lastTime=now;
+//             fn()
+//         }
+//     }
+
+// }
+// const ans=throttle(function(){
+//     console.log("api call")
+
+// },10000)
+// setInterval(()=>{
+//     ans()
+// },100)
+
+
+
+
+// function x(){
+
+//     setTimeout(()=>{
+//         console.log(i)
+//     },1000)
+
+//     var i=1;
+
+// }
+// x()
+
+
+
+// console.log({}==={})
+// console.log({}=={})
+
+
+// var a=8000;
+// {
+//     var a=9000
+// }
+// let b=a;
+// {
+//         let b=10000
+// }
+// console.log(b)
+
+
+// let arr=[1,2,3,4]
+// var ans=arr.map(function(value){
+//     return value=5
+// })
+// console.log(ans)
+
+
+
+// let arr=[1,2,3,4,2]
+// let ans=arr.sort(function(a,b){
+//     return a-b
+
+// })
+// let duplicate=[]
+// for(let i=0;i<arr.length;i++){
+//     if(arr[i]==arr[i+1]){
+//         duplicate.push(arr[i])
+//     }
+// }
+// console.log(duplicate)
+
+
+// var arr=[1,2,3,4,5]
+// function reversearray(arr){
+//     return arr.reverse()
+
+
+// }
+// console.log(reversearray(arr))
+
+
+
+// var arr=[1,2,3,4,5]
+// var ans=[]
+// function reverse(arr){
+//     for(var i=arr.length-1;i>=0;i--){
+
+//         ans.push(arr[i])
+
+
+//     }
+//     return ans
+// }
+// console.log(reverse(arr))
+
+
+
+
+var arr=[1,2,3,4,5]
+
+var ans=arr.sort(function(a,b){
+    return a-b
+})
+console.log(arr[0])
